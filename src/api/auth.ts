e918796d6c6f6d4ad6dd2f85eaefc73025532aa4
gordonchis1 <eduardovaldse@yahoo.com>
2026-08-20 17:10:11 -0600

    Add tests for getAPIKey function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,15 +1,16 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorization"];
-  if (!authHeader) {
-    return null;
-  }
+    const authHeader = headers["authorization"];
+    if (!authHeader) {
+        return null;
+    }
 
-  const splitAuth = authHeader.split(" ");
-  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    return null;
-  }
+    const splitAuth = authHeader.split(" ");
+    if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+        return null;
+    }
 
-  return splitAuth[1];
+    // return splitAuth[1];
+    return "hola mundo";
 }
---
+++ src/tests/auth.test.ts
@@ -0,0 +1,24 @@
+import { getAPIKey } from "../api/auth.js";
+import { describe, expect, it } from "vitest";
+
+describe("Test getAPIKey function", () => {
+    it("Send valid apiKey header", () => {
+        const headers = {
+            "authorization": "ApiKey 123"
+        }
+
+        const key = getAPIKey(headers);
+
+        expect(key).toBe("123");
+    })
+    it("Send invalid token header", () => {
+        const headers = {
+            "authorization": "123"
+        }
+
+        const key = getAPIKey(headers);
+
+        expect(key).toBe(null);
+    })
+
+})
