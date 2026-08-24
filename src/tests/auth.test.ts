e2ccd7e309ec4c42c05491976fa50be31876f21b
gordonchis1 <eduardovaldse@yahoo.com>
2026-08-24 10:44:26 -0600

    Add: prettier and format check in ci workflow

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,23 +2,22 @@ import { getAPIKey } from "../api/auth.js";
 import { describe, expect, it } from "vitest";
 
 describe("Test getAPIKey function", () => {
-    it("Send valid apiKey header", () => {
-        const headers = {
-            "authorization": "ApiKey 123"
-        }
+  it("Send valid apiKey header", () => {
+    const headers = {
+      authorization: "ApiKey 123",
+    };
 
-        const key = getAPIKey(headers);
+    const key = getAPIKey(headers);
 
-        expect(key).toBe("123");
-    })
-    it("Send invalid token header", () => {
-        const headers = {
-            "authorization": "123"
-        }
+    expect(key).toBe("123");
+  });
+  it("Send invalid token header", () => {
+    const headers = {
+      authorization: "123",
+    };
 
-        const key = getAPIKey(headers);
+    const key = getAPIKey(headers);
 
-        expect(key).toBe(null);
-    })
-
-})
+    expect(key).toBe(null);
+  });
+});
